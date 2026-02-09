b80d96d75db12fadf303575c4b3cd8f0664dc2c2
akrep0121 <soneryilmaz021@gmail.com>
2026-02-09 23:44:42 +0300

    Changed target bot to ucretsizderinlikbot

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
     API_HASH: process.env.API_HASH,
     BOT_TOKEN: process.env.BOT_TOKEN,
     CHANNEL_ID: process.env.CHANNEL_ID, // Optional: for broadcasting
-    TARGET_BOT_USERNAME: "xFinansBeta_bot"
+    TARGET_BOT_USERNAME: "ucretsizderinlikbot"
 };
